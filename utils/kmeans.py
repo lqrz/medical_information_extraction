187d3fb6592c75229d881f46516b2fe9fc7327b0
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-03-23 13:07:18 +0100

    use utils functions

--- utils/kmeans.py
+++ utils/kmeans.py
@@ -2,15 +2,12 @@ __author__ = 'root'
 
 from sklearn.cluster import KMeans
 import logging
-import gensim
 from joblib import dump
 from data import get_w2v_model
 from data.dataset import Dataset
 from nltk import word_tokenize
 import numpy as np
-
-def load_w2v(model_filename):
-    return gensim.models.Word2Vec.load_word2vec_format(model_filename, binary=True)
+import utils
 
 def filter_w2v_model(w2v_model):
     training_data_filename = 'handoverdata.zip'
@@ -29,7 +26,9 @@ def filter_w2v_model(w2v_model):
 
     matrix = np.zeros((len(vectors.keys()), w2v_model.syn0.shape[1]))
 
-    for i,vec in vectors.iteritems():
+    print matrix.shape
+
+    for i,vec in enumerate(vectors.values()):
         matrix[i] = vec
 
     return matrix
@@ -44,7 +43,7 @@ if __name__ == '__main__':
 
     logger.info('Loading W2V model')
     W2V_PRETRAINED_FILENAME = 'GoogleNews-vectors-negative300.bin.gz'
-    w2v_model = load_w2v(get_w2v_model(W2V_PRETRAINED_FILENAME))
+    w2v_model = utils.Word2Vec.load_w2v(get_w2v_model(W2V_PRETRAINED_FILENAME))
 
     logger.info('Filtering w2v model')
     filtered_w2v_model = filter_w2v_model(w2v_model)
