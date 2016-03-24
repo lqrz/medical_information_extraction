ea14ca0d8449c2124ccbb4f159f4ce613d1ab952
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-03-24 11:42:22 +0100

    train w2v model

--- data/__init__.py
+++ data/__init__.py
@@ -15,3 +15,6 @@ def get_wikipedia_file(file_name):
 
 def get_w2v_training_data_vectors():
     return pkg_resources.resource_filename('data', 'word2vec/w2v_representations.p')
+
+def get_w2v_directory(file_name):
+    return pkg_resources.resource_filename('data', 'word2vec/%s' % file_name)
