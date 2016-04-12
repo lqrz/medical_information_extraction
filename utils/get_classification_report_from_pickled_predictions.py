f772229abe9b1a2953ddafb63381a02348465ca6
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-04-12 12:40:58 +0200

    index2labels not required

--- utils/get_classification_report_from_pickled_predictions.py
+++ utils/get_classification_report_from_pickled_predictions.py
@@ -11,7 +11,7 @@ import argparse
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Evaluate pickled results')
     parser.add_argument('--file', type=str, required=True, help='Pickle file with predictions.')
-    parser.add_argument('--index2labels', type=str, required=True, help='Pickle file with mapping.')
+    parser.add_argument('--index2labels', type=str, default=False, help='Pickle file with mapping.')
     parser.add_argument('--oldv', action='store_true', help='Old version pickle?.')
     arguments = parser.parse_args()
     path = arguments.file
@@ -19,7 +19,6 @@ if __name__=='__main__':
     index2labels_path = arguments.index2labels
 
     p = cPickle.load(open(path, 'rb'))
-    index2labels = cPickle.load(open(index2labels_path, 'rb'))
 
     #TODO: this is horrible. Consistency!
     if old_version:
@@ -29,5 +28,9 @@ if __name__=='__main__':
         y_t = list(chain(*[trues[0] for trues in p.values()]))
         y_p = list(chain(*[trues[1] for trues in p.values()]))
 
-    print Metrics.compute_classification_report(y_t, y_p, labels=index2labels.keys(),
-                                                target_names=index2labels.values())
+    if index2labels_path:
+        index2labels = cPickle.load(open(index2labels_path, 'rb'))
+        print Metrics.compute_classification_report(y_t, y_p, labels=index2labels.keys(),
+                                                    target_names=index2labels.values())
+    else:
+        print Metrics.compute_classification_report(y_t, y_p)
