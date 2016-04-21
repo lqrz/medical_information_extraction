5a9f0a9faf80f3ca5caab6d1cc48ed51ba1ef142
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-04-21 11:25:35 +0200

    context window recurrent net.

--- SOTA/neural_network/recurrent_Context_Window_net.py
+++ SOTA/neural_network/recurrent_Context_Window_net.py
@@ -31,14 +31,13 @@ print theano.config.floatX
 
 INT = 'int64'
 
-class Recurrent_net(A_neural_network):
+class Recurrent_Context_Window_net(A_neural_network):
 
     def __init__(self, hidden_activation_f, out_activation_f, regularization, **kwargs):
-        super(Recurrent_net, self).__init__(**kwargs)
+        super(Recurrent_Context_Window_net, self).__init__(**kwargs)
 
         self.hidden_activation_f = hidden_activation_f
         self.out_activation_f = out_activation_f
-        self.n_window = 1
 
         self.regularization = regularization
 
@@ -342,7 +341,7 @@ class Recurrent_net(A_neural_network):
 
         y = T.vector(name='y', dtype=INT)
 
-        idxs = T.vector(name="idxs", dtype=INT)  # columns: context window size/lines: tokens in the sentence
+        idxs = T.matrix(name="idxs", dtype=INT)  # columns: context window size/lines: tokens in the sentence
         n_tokens = idxs.shape[0]  # tokens in sentence
 
         w1 = theano.shared(value=np.array(self.pretrained_embeddings).astype(dtype=theano.config.floatX), name='w1',
@@ -394,9 +393,6 @@ class Recurrent_net(A_neural_network):
 
         out_bidirectional = self.out_activation_f(T.dot(h_bidirectional, self.params['w2']) + self.params['b2'])
 
-        # test = theano.function(inputs=[idxs], outputs=[out_bidirectional], allow_input_downcast=True)
-        # test(self.x_train[0])
-
         if self.regularization:
             L2 = T.sum(w1[idxs] ** 2) + T.sum(w2 ** 2) + T.sum(ww_forward ** 2) + T.sum(ww_backwards ** 2)
             cost = T.mean(T.nnet.categorical_crossentropy(out_bidirectional, y)) + alpha_l2_reg * L2
@@ -410,13 +406,23 @@ class Recurrent_net(A_neural_network):
         # test_probs(self.x_train[0], self.y_train[0])
 
         grads = [T.grad(cost, self.params[param_name]) for param_name in grad_params_names]
+        # grad_0 = grads[0]
+        # grad_1 = grads[1]
+        # grad_1_flip = grad_1[::-1]
+        # grad_1_res = grad_1_flip.reshape(
+        #          shape=(self.n_window * n_tokens, self.n_emb,1), ndim=3)
 
+        #TODO: keep testing this. is the reshape gonna update the correct word embedding indexes?
         tmp_grads = OrderedDict(zip(grad_params_names, grads))
         # compute w_x aggregated gradient.
-        w_x_agg_grad = T.sum(T.concatenate(
-            [tmp_grads['w_x'].reshape(shape=(tmp_grads['w_x'].shape[0], tmp_grads['w_x'].shape[1], 1), ndim=3),
+        w_x_agg_grad = T.mean(T.concatenate(
+            [tmp_grads['w_x'].reshape(shape=(self.n_window * n_tokens, self.n_emb,1), ndim=3),
              tmp_grads['w_x_flipped'][::-1].reshape(
-                 shape=(tmp_grads['w_x_flipped'].shape[0], tmp_grads['w_x_flipped'].shape[1], 1), ndim=3)], axis=2), axis=2)
+                 shape=(self.n_window * n_tokens, self.n_emb,1), ndim=3)], axis=2), axis=2)
+
+        # test = theano.function(inputs=[idxs, y], outputs=[w_x_agg_grad],
+        #                        allow_input_downcast=True)
+        # test(self.x_train[0], self.y_train[0])
 
         grad_params_names_agg = ['w_x', 'b1', 'w2', 'b2', 'ww_forward', 'ww_backwards']
         grads_agg = [w_x_agg_grad, tmp_grads['b1'], tmp_grads['w2'], tmp_grads['b2'], tmp_grads['ww_forward'], tmp_grads['ww_backwards']]
@@ -431,22 +437,27 @@ class Recurrent_net(A_neural_network):
             accumulated_grad.append(theano.shared(value=eps, name=param_name, borrow=True))
 
         # grad = grads_agg[0]
-        # param = self.params['w_x']
-        # accum = T.inc_subtensor(accumulated_grad[0][idxs], T.sqr(grad))
-        # upd = - learning_rate * grad / (T.sqrt(accum[idxs]) + 10 ** -5)
+        # idxs_r = idxs.reshape(shape=(-1,))
+        # param = self.params['w1'][idxs_r]
+        # ac = accumulated_grad[0][idxs_r]
+        # accum = T.inc_subtensor(ac, T.sqr(grad))
+        # upd = - learning_rate * grad / (T.sqrt(accum[idxs_r]) + 10 ** -5)
         # update = T.inc_subtensor(param, upd)
         #
-        # test = theano.function([idxs,y], [grad,param,accum,upd,update], allow_input_downcast=True)
+        # test = theano.function([idxs,y], [w_x,idxs_r,param,ac,accum,upd,update], allow_input_downcast=True, on_unused_input='ignore')
         # test(self.x_train[0], self.y_train[0])
 
+        # for reference: grad_params_names_agg = ['w_x', 'b1', 'w2', 'b2', 'ww_forward', 'ww_backwards']
         updates = []
         for param_name, grad, accum_grad in zip(grad_params_names_agg, grads_agg, accumulated_grad):
             param = self.params[param_name]
             if param_name == 'w_x':
+                idxs_r = idxs.reshape(shape=(-1,))
+                param = self.params['w1'][idxs_r]
                 # this will return the whole accum_grad structure incremented in the specified idxs
-                accum = T.inc_subtensor(accum_grad[idxs], T.sqr(grad))
+                accum = T.inc_subtensor(accum_grad[idxs_r], T.sqr(grad))
                 # this will return the whole w1 structure decremented according to the idxs vector.
-                update = T.inc_subtensor(param, - learning_rate * grad / (T.sqrt(accum[idxs]) + 10 ** -5))
+                update = T.inc_subtensor(param, - learning_rate * grad / (T.sqrt(accum[idxs_r]) + 10 ** -5))
                 # update whole structure with whole structure
                 updates.append((self.params['w1'], update))
                 # update whole structure with whole structure
@@ -538,7 +549,7 @@ class Recurrent_net(A_neural_network):
 
     def predict_bidirectional_with_shared_params(self, **kwargs):
 
-        idxs = T.vector(name="test_idxs", dtype=INT)  # columns: context window size/lines: tokens in the sentence
+        idxs = T.matrix(name="test_idxs", dtype=INT)  # columns: context window size/lines: tokens in the sentence
         n_emb = self.pretrained_embeddings.shape[1]  # embeddings dimension
         n_tokens = idxs.shape[0]  # tokens in sentence
 
@@ -583,7 +594,7 @@ class Recurrent_net(A_neural_network):
 
     def predict_bidirectional_without_shared_params(self, **kwargs):
 
-        idxs = T.vector(name="test_idxs", dtype=INT)  # columns: context window size/lines: tokens in the sentence
+        idxs = T.matrix(name="test_idxs", dtype=INT)  # columns: context window size/lines: tokens in the sentence
         n_emb = self.pretrained_embeddings.shape[1]  # embeddings dimension
         n_tokens = idxs.shape[0]  # tokens in sentence
 
@@ -630,6 +641,51 @@ class Recurrent_net(A_neural_network):
         return 'RNN.'
 
     @classmethod
+    def _get_partitioned_data_with_context_window(cls, doc_sentences, doc_sentences_tags,
+                                                  n_window, word2index, label2index):
+        x = []
+        for sentence in doc_sentences:
+            x.append([map(lambda x: word2index[x], sent_window)
+                      for sent_window in utils.NeuralNetwork.context_window(sentence, n_window)])
+
+        y = []
+        for sent in doc_sentences_tags:
+            y.append(map(lambda x: label2index[x], sent))
+
+        return x, y
+
+    @classmethod
+    def get_partitioned_data(cls, x_idx, document_sentences_words, document_sentences_tags,
+                             word2index, label2index, use_context_window=False, n_window=None):
+        """
+        this is at sentence level.
+        it partitions the training data according to the x_idx doc_nrs used for training and y_idx doc_nrs used for
+        testing while cross-validating.
+
+        :param x_idx:
+        :param y_idx:
+        :return:
+        """
+        x = []
+        y = []
+
+        for doc_nr, doc_sentences in document_sentences_words.iteritems():
+            if (not x_idx) or (doc_nr in x_idx):
+                # training set
+                if use_context_window:
+                    x_doc, y_doc = cls._get_partitioned_data_with_context_window(doc_sentences,
+                                                                                 document_sentences_tags[doc_nr],
+                                                                                 n_window, word2index, label2index)
+                else:
+                    x_doc, y_doc = cls._get_partitioned_data_without_context_window(doc_sentences,
+                                                                                    document_sentences_tags[doc_nr],
+                                                                                    word2index, label2index)
+                x.extend(x_doc)
+                y.extend(y_doc)
+
+        return x, y
+
+    @classmethod
     def get_data(cls, crf_training_data_filename, testing_data_filename=None, add_tags=[], x_idx=None, n_window=None):
         """
         overrides the inherited method.
@@ -655,131 +711,22 @@ class Recurrent_net(A_neural_network):
                                                                                 document_sentence_tags)
 
         x_train, y_train = cls.get_partitioned_data(x_idx=x_idx,
-                                 document_sentences_words=train_document_sentence_words,
-                                 document_sentences_tags=train_document_sentence_tags,
-                                 word2index=word2index,
-                                 label2index=label2index,
-                                 use_context_window=False,
-                                 n_window=n_window)
+                                                    document_sentences_words=train_document_sentence_words,
+                                                    document_sentences_tags=train_document_sentence_tags,
+                                                    word2index=word2index,
+                                                    label2index=label2index,
+                                                    use_context_window=True,
+                                                    n_window=n_window)
 
         x_test, y_test = cls.get_partitioned_data(x_idx=x_idx,
-                                 document_sentences_words=test_document_sentence_words,
-                                 document_sentences_tags=test_document_sentence_tags,
-                                 word2index=word2index,
-                                 label2index=label2index,
-                                 use_context_window=False,
-                                 n_window=n_window)
+                                              document_sentences_words=test_document_sentence_words,
+                                              document_sentences_tags=test_document_sentence_tags,
+                                              word2index=word2index,
+                                              label2index=label2index,
+                                              use_context_window=True,
+                                              n_window=n_window)
 
         return x_train, y_train, x_test, y_test, word2index, index2word, label2index, index2label
 
-if __name__ == '__main__':
-    crf_training_data_filename = 'handoverdata.zip'
-
-    training_vectors_filename = get_w2v_training_data_vectors()
-
-    #TODO: make param.
-    use_context_window = False
-    n_window = 1
-
-    w2v_vectors = None
-    w2v_model = None
-    w2v_dims = None
-    if os.path.exists(training_vectors_filename):
-        logger.info('Loading W2V vectors from pickle file')
-        w2v_vectors = cPickle.load(open(training_vectors_filename,'rb'))
-        w2v_dims = len(w2v_vectors.values()[0])
-    else:
-        logger.info('Loading W2V model')
-        W2V_PRETRAINED_FILENAME = 'GoogleNews-vectors-negative300.bin.gz'
-        w2v_model = utils.Word2Vec.load_w2v(get_w2v_model(W2V_PRETRAINED_FILENAME))
-        w2v_dims = w2v_model.syn0.shape[0]
-
-    logger.info('Loading CRF training data')
-
-    #TODO: if i tokenize myself, i get a diff split
-    # training_sentences = Dataset.get_words_in_training_dataset(crf_training_data_filename)
-
-    # crf_training_dataset,_ = Dataset.get_crf_training_data(crf_training_data_filename)
-
-    # annotated_data = transform_crf_training_data(crf_training_dataset)
-    # training_sentence_words/ = [word['word'] for archive in crf_training_dataset.values() for word in archive.values()]
-    # training_sentence_tags = [word['tag'] for archive in crf_training_dataset.values() for word in archive.values()]
-
-    # sentences_words, sentences_tags = Dataset.get_training_file_tokenized_sentences(crf_training_data_filename)
-    _, _, document_sentence_words, document_sentence_tags = Dataset.get_crf_training_data_by_sentence(crf_training_data_filename)
-
-    unique_words = list(set([word for doc_sentences in document_sentence_words.values() for sentence in doc_sentences for word in sentence]))
-    unique_labels = list(set([tag for doc_sentences in document_sentence_tags.values() for sentence in doc_sentences for tag in sentence]))
-    # unique_words = list(set([word_dict['word'] for doc_sentences in document_sentence.values() for sentence in doc_sentences for word_dict in sentence]))
-    # unique_labels = list(set([word_dict['tag'] for doc_sentences in document_sentence.values() for sentence in doc_sentences for word_dict in sentence]))
-
-    logger.info('Creating word-index dictionaries')
-    index2word = defaultdict(None)
-    word2index = defaultdict(None)
-    word2index['<PAD>'] = len(unique_words)
-    for i,word in enumerate(unique_words):
-        index2word[i] = word
-        word2index[word] = i
-
-    logger.info('Creating label-index dictionaries')
-    index2label = defaultdict(None)
-    label2index = defaultdict(None)
-    label2index['<PAD>'] = len(unique_labels)
-    for i,label in enumerate(unique_labels):
-        index2label[i] = label
-        label2index[label] = i
-
-    n_out = len(label2index)
-
-    n_unique_words = len(unique_words)+1    # +1 for the <PAD>
-    n_labels = len(unique_labels)+1
-
-    lim = np.sqrt(6./(n_unique_words+w2v_dims))
-
-    w = np.random.uniform(-lim,lim,(n_unique_words,w2v_dims))
-
-    w = utils.NeuralNetwork.replace_with_word_embeddings(w, unique_words, w2v_vectors=w2v_vectors, w2v_model=w2v_model)
-
-    words_per_document, tags_per_document, document_sentences_words, document_sentences_tags, n_docs, unique_words, unique_labels, index2word, word2index, index2label, label2index, n_unique_words, n_out = RNN_trainer.get_data(crf_training_data_filename)
-
-    logger.info('Instantiating RNN')
-    params = {
-    'hidden_activation_f': utils.NeuralNetwork.tanh_activation_function,
-    'out_activation_f': utils.NeuralNetwork.softmax_activation_function,
-    'n_window': n_window,
-    'words_per_document': None,
-    'tags_per_document': None,
-    'document_sentences_words': document_sentence_words,
-    'document_sentences_tags': document_sentence_tags,
-    'unique_words': unique_words,
-    'unique_labels': unique_labels,
-    'index2word': index2word,
-    'word2index': word2index,
-    'index2label': index2label,
-    'label2index': label2index,
-    'n_unique_words': n_unique_words,
-    'n_out': n_out
-    }
-    nn_trainer = RNN_trainer(**params)
-
-    #TODO: change to make sentences.
-    if use_context_window:
-        nn_trainer.x_train = np.array([map(lambda x: word2index[x],sentence_window) for sentences in document_sentence_words.values()
-                   for sentence in sentences
-                   for sentence_window in utils.NeuralNetwork.context_window(sentence,n_window)])
-    else:
-        # x_train = np.array([map(lambda x: word2index[x], sentence) for sentence in sentences_words.values()])
-        # x_train = np.array([map(lambda x: word2index[x], sentence) for sentence in delete_words])
-        # x_train = np.array([map(lambda x: word2index[x], sentence) for doc_sentences in sentences_words.values() for sentence in doc_sentences])
-        nn_trainer.x_train = np.array([map(lambda x:word2index[x],sentence) for doc_sentences in document_sentence_words.values() for sentence in doc_sentences])
-
-    # y_train = np.array([map(lambda x: label2index[x], sentence) for sentence in delete_tags])
-    # y_train = np.array([map(lambda x: label2index[x], sentence) for doc_sentences in sentences_tags.values() for sentence in doc_sentences])
-    nn_trainer.y_train = np.array([map(lambda x:label2index[x],sentence) for doc_sentences in document_sentence_tags.values() for sentence in doc_sentences])
-
-    nn_trainer.initialize_w(w2v_dims, w2v_vectors=w2v_vectors, w2v_model=w2v_model)
-
-    logger.info('Training RNN')
-    nn_trainer.train(learning_rate=.01, batch_size=512, max_epochs=50, save_params=False)
-
-    logger.info('End')
+if __name__=='__main__':
+    Recurrent_Context_Window_net.get_data()
