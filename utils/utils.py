bb6110e23f2133babf1447f59dbe80eb886e3d15
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-05-12 16:17:39 +0200

    gpu optimized theano.tensor.concatenate

--- utils/utils.py
+++ utils/utils.py
@@ -48,6 +48,55 @@ class NeuralNetwork:
         return [l_padded[i:(i+n_window)] for i in range(len(sentence))]
 
     @staticmethod
+    def theano_gpu_concatenate(tensor_list, axis=0):
+        """
+        Alternative implementation of `theano.tensor.concatenate`.
+        This function does exactly the same thing, but contrary to Theano's own
+        implementation, the gradient is implemented on the GPU.
+        Backpropagating through `theano.tensor.concatenate` yields slowdowns
+        because the inverse operation (splitting) needs to be done on the CPU.
+        This implementation does not have that problem.
+        :usage:
+            x, y = theano.tensor.matrices('x', 'y')
+            c = concatenate([x, y], axis=1)
+        :parameters:
+            - tensor_list : list
+                list of Theano tensor expressions that should be concatenated.
+            - axis : int
+                the tensors will be joined along this axis.
+        :returns:
+            - out : tensor
+                the concatenated tensor expression.
+        """
+
+        import theano
+        import theano.tensor as tensor
+
+        concat_size = sum(tt.shape[axis] for tt in tensor_list)
+
+        output_shape = ()
+        for k in range(axis):
+            output_shape += (tensor_list[0].shape[k],)
+        output_shape += (concat_size,)
+        for k in range(axis + 1, tensor_list[0].ndim):
+            output_shape += (tensor_list[0].shape[k],)
+
+        out = tensor.zeros(output_shape, dtype=tensor_list[0].dtype)
+        offset = 0
+        for tt in tensor_list:
+            indices = ()
+            for k in range(axis):
+                indices += (slice(None),)
+            indices += (slice(offset, offset + tt.shape[axis]),)
+            for k in range(axis + 1, tensor_list[0].ndim):
+                indices += (slice(None),)
+
+            out = tensor.set_subtensor(out[indices], tt)
+            offset += tt.shape[axis]
+
+        return out
+
+    @staticmethod
     def initialize_weights(n_in, n_out, function):
         lim = np.sqrt(6./(n_in+n_out))
 
