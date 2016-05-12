f5163693180627aafa18ca71974f98d41e3a5934
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-05-12 16:26:18 +0200

    initial tensorflow test

--- test/tensorflow_test.py
+++ test/tensorflow_test.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
 
-if __name__=='__main__':
-    matrix1 = tf.constant([[3.,3.]])
-    matrix2 = tf.constant([[2.,2.]])
+def matrix_multiplication():
+    m1 = tf.constant([[3.,3.]])
+    m2 = tf.constant([[2.,2.]])
 
-    print tf.matmul(m1, m2)
+    return tf.matmul(m1, tf.transpose(m2))
+
+if __name__=='__main__':
+    ses = tf.Session()
+    print ses.run(matrix_multiplication())
