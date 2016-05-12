020a2059a6c463b6571c4429a6e6c3670b96e7ed
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-05-12 16:25:32 +0200

    separated validation from testing set. save to file for challenge

--- SOTA/ensemble/train_ensemble_forest_mlp.py
+++ SOTA/ensemble/train_ensemble_forest_mlp.py
@@ -27,6 +27,7 @@ from data import get_w2v_model
 from utils.metrics import Metrics
 from utils.plot_confusion_matrix import plot_confusion_matrix
 from data import get_classification_report_labels
+from data.dataset import Dataset
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -129,7 +130,7 @@ def load_w2v_model_and_vectors_cache(args):
 
     return w2v_vectors, w2v_model, w2v_dims
 
-def filter_tags_to_predict(y_train_labels, y_test_labels, tags, default_tag=None):
+def filter_tags_to_predict(y_train_labels, y_valid_labels, y_test_labels, tags, default_tag=None):
 
     #recreate indexes so they are continuous and start from 0.
     new_index2label = dict()
@@ -153,33 +154,41 @@ def filter_tags_to_predict(y_train_labels, y_test_labels, tags, default_tag=None
         return new_index
 
     y_train = []
+    y_valid = []
     y_test = []
     if isinstance(y_train_labels[0], list):
         for i, sent in enumerate(y_train_labels):
             y_train.append(map(replace_tag, sent))
+        for i, sent in enumerate(y_valid_labels):
+            y_valid.append(map(replace_tag, sent))
         for i, sent in enumerate(y_test_labels):
             y_test.append(map(replace_tag, sent))
     else:
         y_train = map(replace_tag, y_train_labels)
+        y_valid = map(replace_tag, y_valid_labels)
         y_test = map(replace_tag, y_test_labels)
 
-    return y_train, y_test, new_label2index, new_index2label
+    return y_train, y_valid, y_test, new_label2index, new_index2label
 
-def get_original_labels(y_test, y_train, index2label):
+def get_original_labels(y_train, y_valid, y_test, index2label):
     y_train_labels = []
+    y_valid_labels = []
     y_test_labels = []
 
     # recurrent nets use lists of lists.
     if isinstance(y_train[0], list):
         for i, sent in enumerate(y_train):
             y_train_labels.append(map(lambda x: index2label[x], sent))
+        for i, sent in enumerate(y_valid):
+            y_valid_labels.append(map(lambda x: index2label[x], sent))
         for i, sent in enumerate(y_test):
             y_test_labels.append(map(lambda x: index2label[x], sent))
     else:
         y_train_labels = map(lambda x: index2label[x], y_train)
-        y_test_labels = map(lambda x: index2label[x], y_test)
+        y_valid_labels = map(lambda x: index2label[x], y_valid)
+        y_test_labels = map(lambda x: index2label[x] if x else None, y_test)
 
-    return y_train_labels, y_test_labels
+    return y_train_labels, y_valid_labels, y_test_labels
 
 def determine_nnclass_and_parameters(args):
 
@@ -217,9 +226,7 @@ def determine_nnclass_and_parameters(args):
 
     return nn_class, hidden_f, out_f, add_words, add_tags, tag_dim, net_window
 
-def use_testing_dataset(crf_training_data_filename,
-                        test_data_filename,
-                        nn_class,
+def use_testing_dataset(nn_class,
                         hidden_f,
                         out_f,
                         w2v_model,
@@ -245,48 +252,65 @@ def use_testing_dataset(crf_training_data_filename,
 
     logger.info('Loading CRF training data')
 
-    x_train, y_train_all, x_test, y_test_all, word2index_all, index2word_all, label2index_all, index2label_all = \
-        A_neural_network.get_data(crf_training_data_filename, test_data_filename, add_words, add_tags,
-                                  x_idx=None, n_window=forest_window)
+    x_train, y_train_all, x_valid, y_valid_all, x_test, y_test_all, word2index_all, index2word_all, label2index_all, index2label_all = \
+        nn_class.get_data(clef_training=True, clef_validation=True, clef_testing=True, add_words=add_words,
+                          add_tags=add_tags, x_idx=None, n_window=forest_window)
 
     unique_words = word2index_all.keys()
 
     pretrained_embeddings = A_neural_network.initialize_w(w2v_dims, unique_words, w2v_vectors=w2v_vectors, w2v_model=w2v_model)
 
     # determine which tags to predict in the first step, and which ones in the second.
-    y_train_labels, y_test_labels = get_original_labels(y_test_all, y_train_all, index2label_all)
+    y_train_labels, y_valid_labels, y_test_labels = get_original_labels(y_train_all, y_valid_all, y_test_all, index2label_all)
     tags = get_param(tags)
     tags_2nd_step = set(label2index_all.keys()) - set(tags)
     default_tag = '<OTHER>'
-    y_train_1st_step, y_test_1st_step, label2index_1st_step, index2label_1st_step = \
-        filter_tags_to_predict(y_train_labels, y_test_labels, tags, default_tag=default_tag)
+    y_train_1st_step, y_valid_1st_step, y_test_1st_step, label2index_1st_step, index2label_1st_step = \
+        filter_tags_to_predict(y_train_labels, y_valid_labels, y_test_labels, tags, default_tag=default_tag)
 
     forest = Forest(classifier, pretrained_embeddings, forest_window)
     forest.train(x_train, y_train_1st_step)
-    predictions_1st_step = forest.predict(x_test)
+    train_predictions_1st_step = forest.predict(x_train)
+    valid_predictions_1st_step = forest.predict(x_valid)
+    test_predictions_1st_step = forest.predict(x_test)
 
-    score_1st_step = Metrics.compute_all_metrics(y_test_1st_step, predictions_1st_step, average=None)
-    print score_1st_step
+    assert y_train_1st_step.__len__() == train_predictions_1st_step.__len__()
+    train_score_1st_step = Metrics.compute_all_metrics(y_train_1st_step, train_predictions_1st_step, average=None)
+    print 'Training scores'
+    print train_score_1st_step
+
+    assert y_valid_1st_step.__len__() == valid_predictions_1st_step.__len__()
+    valid_score_1st_step = Metrics.compute_all_metrics(y_valid_1st_step, valid_predictions_1st_step, average=None)
+    print 'Validation scores'
+    print valid_score_1st_step
 
     if forest_window != net_window:
         # the forest and the nnet might use different window sizes.
-        x_train, _, x_test, _, _, _, _, _ = \
-            A_neural_network.get_data(crf_training_data_filename, test_data_filename, add_words, add_tags,
-                                      x_idx=None, n_window=net_window)
+
+        x_train, _, x_valid, _, x_test, _, _, _, _, _ = \
+            nn_class.get_data(clef_training=True, clef_validation=True, clef_testing=True, add_words=add_words,
+                              add_tags=add_tags, x_idx=None, n_window=net_window)
+        # x_train, _, x_test, _, _, _, _, _ = \
+        #     A_neural_network.get_data(crf_training_data_filename, test_data_filename, add_words, add_tags,
+        #                               x_idx=None, n_window=net_window)
 
     #--- here starts the 2nd step ---#
 
     # get the subsamples to train and test on this 2nd step (all that were marked as '<OTHER>').
     indexes_to_train = np.where(np.array(y_train_1st_step) == label2index_1st_step[default_tag])[0]
-    indexes_to_test = np.where(predictions_1st_step == label2index_1st_step[default_tag])[0]
+    indexes_to_valid = np.where(valid_predictions_1st_step == label2index_1st_step[default_tag])[0]
+    indexes_to_test = np.where(test_predictions_1st_step == label2index_1st_step[default_tag])[0]
+    indexes_to_train_to_replace = np.where(train_predictions_1st_step == label2index_1st_step[default_tag])[0]
     x_train_2nd_step = np.array(x_train)[indexes_to_train]
     y_train_2nd_step = np.array(y_train_labels)[indexes_to_train]
+    x_valid_2nd_step = np.array(x_valid)[indexes_to_valid]
+    y_valid_2nd_step = np.array(y_valid_labels)[indexes_to_valid]
     x_test_2nd_step = np.array(x_test)[indexes_to_test]
     y_test_2nd_step = np.array(y_test_labels)[indexes_to_test]
 
     dummy_tag = '#delete#'  #TODO: this is nasty.
-    y_train_2nd_step, y_test_2nd_step, label2index_2nd_step, index2label_2nd_step = \
-        filter_tags_to_predict(y_train_2nd_step, y_test_2nd_step, tags_2nd_step, default_tag=dummy_tag)
+    y_train_2nd_step, y_valid_2nd_step, y_test_2nd_step, label2index_2nd_step, index2label_2nd_step = \
+        filter_tags_to_predict(y_train_2nd_step, y_valid_2nd_step, y_test_2nd_step, tags_2nd_step, default_tag=dummy_tag)
 
     # TODO: this is nasty.
     dummy_value = label2index_2nd_step[dummy_tag]
@@ -300,6 +324,8 @@ def use_testing_dataset(crf_training_data_filename,
     params = {
         'x_train': x_train_2nd_step,
         'y_train': y_train_2nd_step,
+        'x_valid': x_valid_2nd_step,
+        'y_valid': y_valid_2nd_step,
         'x_test': x_test_2nd_step,
         'y_test': y_test_2nd_step,
         'hidden_activation_f': hidden_f,
@@ -321,27 +347,79 @@ def use_testing_dataset(crf_training_data_filename,
     nn_trainer.train(learning_rate=.01, batch_size=minibatch_size, max_epochs=max_epochs, save_params=False, **kwargs)
 
     logger.info('Predicting tags %s' % ' ,'.join(tags_2nd_step))
-    flat_true_2nd_step, flat_predictions_2nd_step, trues_2nd_step, preds_2nd_step = nn_trainer.predict(**kwargs)
 
-    assert flat_true_2nd_step.__len__() == flat_predictions_2nd_step.__len__()
-    assert indexes_to_test.__len__() == flat_predictions_2nd_step.__len__()
+    logger.info('Predicting on Training set')
+    nn_trainer.x_train = np.array(x_train)[indexes_to_train_to_replace]
+    _, train_flat_predictions_2nd_step, _, _ = nn_trainer.predict(on_training_set=True, **kwargs)
+    # flat_true_2nd_step, train_flat_predictions_2nd_step, trues_2nd_step, preds_2nd_step = nn_trainer.predict(**kwargs)
+
+    logger.info('Predicting on Validation set')
+    valid_flat_true_2nd_step, valid_flat_predictions_2nd_step, _, _ = nn_trainer.predict(on_validation_set=True, **kwargs)
+
+    logger.info('Predicting on Testing set')
+    _, test_flat_predictions_2nd_step, _, _ = nn_trainer.predict(on_validation_set=False, **kwargs)
+
+    assert valid_flat_true_2nd_step.__len__() == valid_flat_predictions_2nd_step.__len__()
+    assert indexes_to_train_to_replace.__len__() == train_flat_predictions_2nd_step.__len__()
+    assert indexes_to_valid.__len__() == valid_flat_predictions_2nd_step.__len__()
+    assert indexes_to_test.__len__() == test_flat_predictions_2nd_step.__len__()
 
     # merge 1st and 2nd steps predictions.
-    final_prediction = map(lambda x: index2label_1st_step[x], predictions_1st_step)
+    train_final_prediction = map(lambda x: index2label_1st_step[x], train_predictions_1st_step)
+    valid_final_prediction = map(lambda x: index2label_1st_step[x], valid_predictions_1st_step)
+    test_final_prediction = map(lambda x: index2label_1st_step[x], test_predictions_1st_step)
     # final_prediction[list(set(range(final_prediction.__len__())) - set(test_index_to_test))] = predictions_1st_step
     # if i index the list as np.array and do the replacement, some tags appear trimmed! I have to forloop them
-    for idx, res in zip(indexes_to_test, map(lambda x: index2label_2nd_step[x], flat_predictions_2nd_step)):
-        final_prediction[idx] = res
+    for idx, res in zip(indexes_to_train_to_replace, map(lambda x: index2label_2nd_step[x], train_flat_predictions_2nd_step)):
+        train_final_prediction[idx] = res
+    for idx, res in zip(indexes_to_valid, map(lambda x: index2label_2nd_step[x], valid_flat_predictions_2nd_step)):
+        valid_final_prediction[idx] = res
+    for idx, res in zip(indexes_to_test, map(lambda x: index2label_2nd_step[x], test_flat_predictions_2nd_step)):
+        test_final_prediction[idx] = res
 
-    assert final_prediction.__len__() == y_test_labels.__len__()
-    assert default_tag not in final_prediction
+    assert train_final_prediction.__len__() == y_train_labels.__len__()
+    assert valid_final_prediction.__len__() == y_valid_labels.__len__()
+    assert test_final_prediction.__len__() == y_test_labels.__len__()
+    assert default_tag not in train_final_prediction
+    assert default_tag not in valid_final_prediction
+    assert default_tag not in test_final_prediction
 
     # flat_true = y_test_labels
     # flat_predictions = map(lambda x: index2label[x], final_prediction)
 
-    results[0] = (y_test_labels, final_prediction)
+    results[0] = (y_valid_labels, valid_final_prediction, train_final_prediction, test_final_prediction)
 
     return results
+
+def write_to_file(fout_name, document_sentence_words, predictions, file_prefix, file_suffix):
+    fout = open(fout_name, 'wb')
+    word_count = 0
+    for doc_nr, sentences in document_sentence_words.iteritems():
+        doc_words = list(chain(*sentences))
+        doc_len = doc_words.__len__()
+        for word, tag in zip(doc_words, predictions[word_count:doc_len]):
+            line = '\t'.join([file_prefix+str(doc_nr)+file_suffix, word, tag])
+            fout.write(line+'\n')
+
+    fout.close()
+
+    return True
+
+def save_predictions_to_file(train_y_pred, valid_y_pred, test_y_pred, get_output_path):
+    train_fout_name = get_output_path('train_B.txt')
+    valid_fout_name = get_output_path('validation_B.txt')
+    test_fout_name = get_output_path('test_B.txt')
+
+    _, _, document_sentence_words, _ = Dataset.get_clef_training_dataset()
+    write_to_file(train_fout_name, document_sentence_words, train_y_pred, file_prefix='output', file_suffix='.txt')
+
+    _, _, document_sentence_words, _ = Dataset.get_clef_validation_dataset()
+    write_to_file(valid_fout_name, document_sentence_words, valid_y_pred, file_prefix='test', file_suffix='.xml.data')
+
+    _, _, document_sentence_words, _ = Dataset.get_clef_testing_dataset()
+    write_to_file(test_fout_name, document_sentence_words, test_y_pred, file_prefix='', file_suffix='.xml.data')
+
+    return True
 
 def perform_leave_one_out():
     raise Exception('Unimplemented.')
@@ -371,33 +449,33 @@ if __name__ == '__main__':
     if args['use_leave_one_out']:
         results = perform_leave_one_out()
     else:
-        results = use_testing_dataset(crf_training_data_filename,
-                                                   test_data_filename,
-                                                   nn_class,
-                                                   hidden_f,
-                                                   out_f,
-                                                   w2v_model,
-                                                   w2v_vectors,
-                                                   w2v_dims,
-                                                   add_words,
-                                                   add_tags,
-                                                   tag_dim,
-                                                   get_output_path,
-                                                   **args)
+        results = use_testing_dataset(nn_class,
+                                        hidden_f,
+                                        out_f,
+                                        w2v_model,
+                                        w2v_vectors,
+                                        w2v_dims,
+                                        add_words,
+                                        add_tags,
+                                        tag_dim,
+                                        get_output_path,
+                                        **args)
 
     cPickle.dump(results, open(get_output_path('prediction_results.p'),'wb'))
     # cPickle.dump(index2label, open(get_output_path('index2labels.p'),'wb'))
 
-    y_true = list(chain(*[true for true, _ in results.values()]))
-    y_pred = list(chain(*[pred for _, pred in results.values()]))
+    valid_y_true = list(chain(*[true for true, _, _, _ in results.values()]))
+    valid_y_pred = list(chain(*[pred for _, pred, _, _ in results.values()]))
+    train_y_pred = list(chain(*[pred for _, _, pred, _ in results.values()]))
+    test_y_pred = list(chain(*[pred for _, _, _, pred in results.values()]))
 
     print '...Plotting confusion matrix'
     output_filename = get_ensemble_forest_mlp_path('confusion_matrix.png')
-    labels_list = list(set(y_true).union(set(y_pred)))
-    cm = Metrics.compute_confusion_matrix(y_true, y_pred, labels=labels_list)
+    labels_list = list(set(valid_y_true).union(set(valid_y_pred)))
+    cm = Metrics.compute_confusion_matrix(valid_y_true, valid_y_pred, labels=labels_list)
     plot_confusion_matrix(confusion_matrix=cm, labels=labels_list, output_filename=output_filename)
 
-    report = Metrics.compute_classification_report(y_true, y_pred, labels=get_classification_report_labels())
+    report = Metrics.compute_classification_report(valid_y_true, valid_y_pred, labels=get_classification_report_labels())
 
     # save classification report to file
     fout = open(get_output_path('classification_report.txt'), 'wb')
@@ -413,13 +491,16 @@ if __name__ == '__main__':
 
     fout.close()
 
-    results_micro = Metrics.compute_all_metrics(y_true, y_pred, average='micro')
-    results_macro = Metrics.compute_all_metrics(y_true, y_pred, average='macro')
+    results_micro = Metrics.compute_all_metrics(valid_y_true, valid_y_pred, average='micro')
+    results_macro = Metrics.compute_all_metrics(valid_y_true, valid_y_pred, average='macro')
 
     print 'MICRO results'
     print results_micro
     print 'MACRO results'
     print results_macro
+
+    print '...Saving predictions to file'
+    save_predictions_to_file(train_y_pred, valid_y_pred, test_y_pred, get_output_path)
 
     print 'Elapsed time: ', time.time()-start
 
