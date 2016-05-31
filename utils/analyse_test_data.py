af31952494f76815baf26a866dd9a97fb9402ec2
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-05-31 20:06:13 +0200

    changed plots. use original embed matrix

--- utils/analyse_test_data.py
+++ utils/analyse_test_data.py
@@ -7,23 +7,21 @@ sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 from collections import defaultdict
 import argparse
 import numpy as np
-import cPickle as pickle
 from sklearn.decomposition import PCA as sklearnPCA
 from itertools import chain
 from ggplot import *
 import pandas as pd
+import cPickle
 
 from data.dataset import Dataset
-from data import get_w2v_training_data_vectors
+from trained_models import get_cwnn_path
+from ggplot_lqrz import ggplot_lqrz
 
 
-def get_test_data(testing_filename):
-    training_data, _, _, _ = Dataset.get_crf_training_data_by_sentence(testing_filename,
-                                                                       path=Dataset.TESTING_FEATURES_PATH + 'test',
-                                                                       extension=Dataset.TESTING_FEATURES_EXTENSION)
+def get_data():
+    training_data, _, document_sentence_words, document_sentence_tags = Dataset.get_clef_training_dataset()
 
-    return training_data
-
+    return training_data, document_sentence_words, document_sentence_tags
 
 def get_words_per_tag(training_data):
 
@@ -40,6 +38,7 @@ def parse_arguments():
     parser.add_argument('--w2vvectorscache', action='store', type=str)
     parser.add_argument('--pca', action='store_true', default=False)
     parser.add_argument('--resize', action='store_true', default=False)
+    parser.add_argument('--model', action='store', choices=['hidden_cw'], required=True)
 
     arguments = parser.parse_args()
 
@@ -47,6 +46,7 @@ def parse_arguments():
     args['w2v_vectors_cache'] = arguments.w2vvectorscache
     args['plot_pca'] = arguments.pca
     args['resize'] = arguments.resize
+    args['model'] = arguments.model
 
     return args
 
@@ -55,41 +55,35 @@ def check_arguments(args):
         print 'Provide w2v vectors for PCA plotting.'
         exit()
 
-def load_w2v_cache(filename):
-    return pickle.load(open(get_w2v_training_data_vectors(filename), 'rb'))
-
-def get_vector_matrix(words_per_tag, w2v_vectors_cache):
+def get_vector_matrix(words_per_tag, word2index, original_embeddings):
 
     representations = defaultdict(lambda : defaultdict(int))
 
     processed_words = []
-    repeated = []
+    # repeated = []
     for tag, words in words_per_tag.iteritems():
         for word in words:
-            try:
-                word = word.lower()
-                rep = w2v_vectors_cache[word]
-                if word in processed_words:
-                    representations[word]['count'] += 1
-                    if (word,tag) not in repeated:
-                        repeated.append((word,tag))
-                else:
-                    representations[word]['rep'] = rep
-                    representations[word]['tag'] = tag
-                    representations[word]['count'] += 1
-                    # reps.append(rep)
-                    # tags.append(tag)
-                    processed_words.append(word)
-            except KeyError:
-                pass
+            # word = word.lower()
+            rep = original_embeddings[word2index[word]]
+            if word in processed_words:
+                representations[word]['count'] += 1
+                # if (word,tag) not in repeated:
+                    # repeated.append((word,tag))
+            else:
+                representations[word]['rep'] = rep
+                representations[word]['tag'] = tag
+                representations[word]['count'] += 1
+                # reps.append(rep)
+                # tags.append(tag)
+                processed_words.append(word)
 
     reps = [d['rep'] for d in representations.values()]
     tags = [d['tag'] for d in representations.values()]
     counts = [d['count'] for d in representations.values()]
 
-    return np.array(reps), np.array(tags), np.array(counts), repeated
+    return np.array(reps), np.array(tags), np.array(counts)
 
-def plot_pca(reps, tags, counts, dimensions=2, resize=False, **kwargs):
+def plot_pca(reps, tags, counts, dimensions, get_output_path, resize=False, title=None, **kwargs):
 
     plt = None
 
@@ -101,39 +95,82 @@ def plot_pca(reps, tags, counts, dimensions=2, resize=False, **kwargs):
             data = np.concatenate([sklearn_transf, np.matrix(tags).T, np.matrix(counts).T], axis=1)
             df = pd.DataFrame(data, columns=['x', 'y', 'tag', 'count'])
 
-            p = ggplot(df, aes(x='x', y='y', color='tag', size='count')) + \
-                  geom_point() + \
-                  labs(x='1st-component', y='2nd-component', title='PCA')
+            p = ggplot_lqrz(df, aes(x='x', y='y', color='tag', size='count')) + \
+                geom_point() + \
+                labs(x='1st-component', y='2nd-component', title='PCA') + \
+                xlim(-5, 5) + \
+                ylim(-5, 5)
         else:
             data = np.concatenate([sklearn_transf, np.matrix(tags).T], axis=1)
             df = pd.DataFrame(data, columns=['x', 'y', 'tag'])
 
-            p = ggplot(df, aes(x='x', y='y', color='tag')) + \
-                  geom_point(size=20) + \
-                  labs(x='1st-component', y='2nd-component', title='PCA')
+            p = ggplot_lqrz(df, aes(x='x', y='y', color='tag')) + \
+                geom_point(size=20) + \
+                labs(x='1st-component', y='2nd-component', title='PCA') + \
+                xlim(-5, 5) + \
+                ylim(-5, 5)
 
-    output_filename = ''.join(['test_dataset_pca-%s', '-resize' if resize else '-no_resize', '.png'])
-    ggsave(output_filename % dimensions, p, dpi=100, bbox_inches='tight')
+
+    output_filename = ''.join(['test_dataset_pca-%s', '-resize' if resize else '-no_resize',
+                               '-', title if title else '', '.png'])
+    ggsave(get_output_path(output_filename % dimensions), p, dpi=100, bbox_inches='tight')
 
     return True
 
+def determine_output_path(args):
+    get_output_path = None
+
+    if args['model'] == 'hidden_cw':
+        get_output_path = get_cwnn_path
+
+    return get_output_path
 
 if __name__ == '__main__':
-    testing_filename = 'handover-set2.zip'
 
     args = parse_arguments()
 
-    check_arguments(args)
+    get_output_path = determine_output_path(args)
 
-    training_data = get_test_data(testing_filename)
+    # check_arguments(args)
 
-    words_per_tag = get_words_per_tag(training_data)
+    _, document_sentence_words, document_sentence_tags = get_data()
+    all_words = list(chain(*chain(*document_sentence_words.values())))
+    all_tags = list(chain(*chain(*document_sentence_tags.values())))
 
-    w2v_vectors_cache = load_w2v_cache(args['w2v_vectors_cache'])
+    word_tags = defaultdict(set)
+    tags_words = defaultdict(list)
+
+    for word, tag in zip(all_words, all_tags):
+        word_tags[word].add(tag)
+        tags_words[tag].append(word)
+
+    tag_overlapping_word_tag = defaultdict(dict)
+    tag_overlapping_tag = defaultdict(dict)
+    tag_overlap = defaultdict(lambda: defaultdict(int))
+
+    words_per_tag = defaultdict(list)
+    for tag, words in tags_words.iteritems():
+        # word_types = set(words)
+        for word in words:
+            if list(word_tags[word]).__len__() > 1:
+                words_per_tag['Overlaped'].append(word)
+            else:
+                words_per_tag[tag].append(word)
+
+    # words_per_tag = get_words_per_tag(validation_data)
+
+    assert all_words.__len__() == np.sum([word_list.__len__() for word_list in words_per_tag.values()])
+
+    # w2v_vectors_cache = Word2Vec.load_w2v(args['w2v_vectors_cache'])
+    word2index = cPickle.load(open(get_output_path('word2index.p'), 'rb'))
+    original_embeddings = cPickle.load(open(get_output_path('original_vectors.p'), 'rb'))
+    trained_embeddings = cPickle.load(open(get_output_path('trained_vectors.p'), 'rb'))
 
     if args['plot_pca']:
-        reps, tags, counts, repeated = get_vector_matrix(words_per_tag, w2v_vectors_cache)
-        plot_pca(reps, tags, counts, dimensions=2, **args)
+        reps, tags, counts = get_vector_matrix(words_per_tag, word2index, original_embeddings)
+        plot_pca(reps, tags, counts, dimensions=2, get_output_path=get_output_path, title='before', **args)
+        reps, tags, counts = get_vector_matrix(words_per_tag, word2index, trained_embeddings)
+        plot_pca(reps, tags, counts, dimensions=2, get_output_path=get_output_path, title='after', **args)
 
     for tag, words in words_per_tag.iteritems():
         print tag+'\t'+', '.join(words)
