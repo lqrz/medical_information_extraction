e88d05cb0b2c546a2ca11176330d11aad5fb6270
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-05-31 20:03:08 +0200

    replace Singleton. plot

--- utils/empirical_distribution.py
+++ utils/empirical_distribution.py
@@ -3,47 +3,13 @@ __author__='lqrz'
 from nltk import FreqDist
 from itertools import chain
 import numpy as np
+from ggplot import *
+import pandas as pd
+from collections import OrderedDict
 
 from data.dataset import Dataset
-
-class Singleton():
-    """
-    A non-thread-safe helper class to ease implementing singletons.
-    This should be used as a decorator -- not a metaclass -- to the
-    class that should be a singleton.
-
-    The decorated class can define one `__init__` function that
-    takes only the `self` argument. Other than that, there are
-    no restrictions that apply to the decorated class.
-
-    To get the singleton instance, use the `Instance` method. Trying
-    to use `__call__` will result in a `TypeError` being raised.
-
-    Limitations: The decorated class cannot be inherited from.
-
-    """
-
-    def __init__(self, decorated):
-        self._decorated = decorated
-
-    def Instance(self):
-        """
-        Returns the singleton instance. Upon its first call, it creates a
-        new instance of the decorated class and calls its `__init__` method.
-        On all subsequent calls, the already created instance is returned.
-
-        """
-        try:
-            return self._instance
-        except AttributeError:
-            self._instance = self._decorated()
-            return self._instance
-
-    def __call__(self):
-        raise TypeError('Singletons must be accessed through `Instance()`.')
-
-    def __instancecheck__(self, inst):
-        return isinstance(inst, self._decorated)
+from data import get_classification_report_labels
+from singleton import Singleton
 
 @Singleton
 class Empirical_distribution():
@@ -89,10 +55,41 @@ class Empirical_distribution():
 
         return self.validation_distribution
 
+def plot_empirical_distribution(distribution, label2index):
+    labels = map(lambda x: label2index[x], [tag for tag,_ in distribution])
+    probs = [prob for _,prob in distribution]
+
+    data = {
+        'labels': labels,
+        'probs': probs
+    }
+
+    df = pd.DataFrame(data)
+    p = ggplot(df, aes(x='labels', weight='probs')) + \
+        geom_bar(stat='bin')
+
+    ggsave('empirical_distribution', p, dpi=100, bbox_inches='tight')
+
 if __name__ == '__main__':
     ed = Empirical_distribution.Instance()
     training_dist = ed._get_training_empirical_distribution()
     validation_dist = ed._get_validation_empirical_distribution()
     sample = [ed.sample_from_training_empirical_distribution() for _ in range(10)]
+
+    data = dict(training_dist)
+    for tag in get_classification_report_labels():
+        try:
+            prob = data[tag]
+            print tag, "{:10.4f}".format(prob)
+        except:
+            continue
+
+    _, _, _, document_sentence_tags = Dataset.get_clef_validation_dataset()
+    tags = list(chain(*(chain(*document_sentence_tags.values()))))
+
+    label2index = OrderedDict()
+    label2index.update(list(zip(set(tags), range(set(tags).__len__()))))
+
+    plot_empirical_distribution(validation_dist, label2index)
 
     print '...End'
