ba97f8cc8809cdbcd7609b83cff7547fd75d0d78
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-06-23 11:29:30 +0200

    minor change

--- test/tensorflow_test.py
+++ test/tensorflow_test.py
@@ -1,11 +1,17 @@
 import tensorflow as tf
 
+import numpy as np
+
 def matrix_multiplication():
     m1 = tf.constant([[3.,3.]])
     m2 = tf.constant([[2.,2.]])
 
     return tf.matmul(m1, tf.transpose(m2))
 
-if __name__=='__main__':
+
+def run_matrix_multiplication():
     ses = tf.Session()
     print ses.run(matrix_multiplication())
+
+if __name__=='__main__':
+    run_matrix_multiplication()
