d87000154e787de32911be31287e389a5c60844b
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-08-19 17:20:15 +0200

    interface Py2R

--- utils/plot_confusion_matrix.py
+++ utils/plot_confusion_matrix.py
@@ -7,23 +7,72 @@ import os
 import six
 import sys
 
-def plot_confusion_matrix(confusion_matrix, labels, output_filename, title=None):
-    df = pd.DataFrame(confusion_matrix, index=labels, columns=labels)
+# def plot_confusion_matrix(confusion_matrix, labels, output_filename, title=None):
+#     df = pd.DataFrame(confusion_matrix, index=labels, columns=labels)
+#     df['true'] = df.index
+#     df_melted = pd.melt(df, id_vars=['true'], var_name='prediction')
+#     plot = ggplot(aes(x='prediction', y='true'), df_melted) + \
+#         custom_geom_tile(aes(fill='value')) + \
+#         labs(x='Predicted labels', y='True labels') + \
+#         scale_color_gradient()
+#
+#     if title:
+#         plot += ggtitle(title)
+#
+#     # plot.__repr__()
+#
+#     #use bbox_inches='tight' for matplotlib to remove margins in image.
+#     ggsave_lqrz(output_filename, plot, dpi=100, bbox_inches='tight')
+
+def plot_confusion_matrix_ggplot(confusion_matrix, labels, output_filename, title=None):
+    import rpy2.robjects as robj
+    import rpy2.robjects.pandas2ri  # for dataframe conversion
+    from rpy2.robjects.packages import importr
+
+    df = pd.DataFrame(confusion_matrix[::-1], index=labels[::-1], columns=labels)
     df['true'] = df.index
     df_melted = pd.melt(df, id_vars=['true'], var_name='prediction')
-    plot = ggplot(aes(x='prediction', y='true'), df_melted) + \
-        custom_geom_tile(aes(fill='value')) + \
-        labs(x='Predicted labels', y='True labels') + \
-        scale_color_gradient()
 
-    if title:
-        plot += ggtitle(title)
+    gr = importr('grDevices')
+    robj.pandas2ri.activate()
+    conv_df = robj.conversion.py2ri(df_melted)
+    plotFunc = robj.r("""
+        library(ggplot2)
 
-    # plot.__repr__()
+        function(df, output_filename){
+            #df$sentence_nr <- as.character(df$sentence_nr)
+            df$true  <- as.character(df$true)
+            df$true  <- factor(df$true, levels=unique(df$true))
+            df$prediction  <- as.character(df$prediction)
+            df$prediction  <- factor(df$prediction, levels=unique(df$prediction))
+            str(df)
 
-    #use bbox_inches='tight' for matplotlib to remove margins in image.
-    ggsave_lqrz(output_filename, plot, dpi=100, bbox_inches='tight')
+            p <- ggplot(df, aes(x=prediction, y=true, fill=value)) +
+                geom_tile(colour='gray92') +
+                # scale_fill_gradient(low='gray99', high='steelblue4', guide = guide_legend(title = "Probability")) +
+                scale_fill_gradient(low='white', high='steelblue', guide = guide_colourbar(title="", ticks=FALSE,
+                                                                                barwidth = 0.5, barheight = 12)) +
+                # scale_fill_gradient() +
+                labs(x='Predicted labels', y='True labels', title='') +
+                theme(
+                    panel.grid.major = element_blank(),
+                    panel.border = element_blank(),
+                    panel.background = element_blank(),
+                    axis.ticks = element_blank(),
+                    axis.text.x = element_text(angle=90, hjust=1, vjust=0.5))
+                    #legend.position="none")
 
+            print(p)
+
+            ggsave(output_filename, plot=p, height=9, width=10, dpi=120)
+
+            }
+        """)
+
+    plotFunc(conv_df, output_filename)
+    gr.dev_off()
+
+    return True
 
 def ggsave_lqrz(filename=None, plot=None, device=None, format=None,
            path=None, scale=1, width=None, height=None, units="in",
