b507d084f02388c08c7e9b5cdbad06379c1efb9e
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-08-19 17:27:05 +0200

    xticks vertical align

--- utils/empirical_distribution.py
+++ utils/empirical_distribution.py
@@ -88,7 +88,7 @@ def plot_empirical_distribution(distribution):
             geom_bar(stat="identity") +
             labs(x='Label', y='Probability', title='Empirical distribution') +
             ylim(0,1) +
-            theme(axis.text.x = element_text(angle = 90, hjust = 1))
+            theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust=0.5))
 
             print(p)
 
