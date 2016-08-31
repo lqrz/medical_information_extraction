ec3e93e951574e9569df09d26f9bf174bacaf6e3
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-08-31 17:24:07 +0200

    cnn learning rate script

--- scripts/cnn_learning_rate.sh
+++ scripts/cnn_learning_rate.sh
@@ -5,7 +5,9 @@ declare -a lr_tune=(0.01 0.001)
 for i in "${lr_train[@]}"
 do
 	for j in "${lr_tune[@]}"
+	do
 		echo "lr_train $i lr_tune $j"
 		stdbuf -oL sudo python SOTA/neural_network/train_neural_network.py --net tf_cnn --window 7 --epochs 20 --plot --lrtrain "$i" --lrtune "$j" --minibatch 256 --configini cnn_w2v_c_m.cfg --earlystop 2 --logger earlystop_lrtrain_"$i"_lrtune_"$j".log
-		stdbuf -oL sudo python SOTA/neural_network/train_neural_network.py --net tf_cnn --window 7 --epochs 20 --plot --lrtrain "$i" --lrtune "$j" --minibatch 256 --configini cnn_w2v_c_m.cfg --picklelists --logger lrtrain_"$i"_lrtune_"$j".log
+		stdbuf -oL sudo python SOTA/neural_network/train_neural_network.py --net tf_cnn --window 7 --epochs 20 --plot --lrtrain "$i" --lrtune "$j" --minibatch 256 --configini cnn_w2v_c_m.cfg --picklelists --logger lrtrain_"$i"_lrtune_"$j".log7
+	done
 done
