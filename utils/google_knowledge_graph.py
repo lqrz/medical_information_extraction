1f48fc266890176a735706f483b5e995ab98faf6
Lautaro Quiroz <lautaro.quiroz@gmail.com>
2016-09-11 17:46:08 +0200

    retrieve test dataset tokens

--- utils/google_knowledge_graph.py
+++ utils/google_knowledge_graph.py
@@ -18,23 +18,20 @@ def get_api_key():
     return open(path).read()
 
 if __name__ == '__main__':
-    crf_training_data_filename = 'handoverdata.zip'
-    crf_testing_data_filename = 'handover-set2.zip'
     output_filename = 'knowledge_graph_cache.p'
 
     api_key = get_api_key()
 
     print 'Retrieving datasets...'
-    _, _, train_document_sentence_words, _ = Dataset.get_crf_training_data_by_sentence(
-        file_name=crf_training_data_filename)
+    _, _, train_document_sentence_words, _ = Dataset.get_clef_training_dataset(lowercase=True)
+    _, _, valid_document_sentence_words, _ = Dataset.get_clef_validation_dataset(lowercase=True)
+    _, _, test_document_sentence_words, _ = Dataset.get_clef_testing_dataset(lowercase=True)
 
-    _, _, test_document_sentence_words, _ = Dataset.get_crf_training_data_by_sentence(
-        file_name=crf_testing_data_filename,
-        path=Dataset.TESTING_FEATURES_PATH + 'test',
-        extension=Dataset.TESTING_FEATURES_EXTENSION)
-
-    train_and_test_words = set(chain(*chain(*train_document_sentence_words.values())))
-    train_and_test_words = train_and_test_words.union(set(chain(*chain(*test_document_sentence_words.values()))))
+    unique_words = set(
+        list(chain(*chain(*train_document_sentence_words.values()))) + \
+        list(chain(*chain(*valid_document_sentence_words.values()))) + \
+        list(chain(*chain(*test_document_sentence_words.values())))
+    )
 
     service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
     params = {
@@ -46,7 +43,7 @@ if __name__ == '__main__':
     results = dict()
 
     print 'Querying knowledge graph...'
-    for word_type in train_and_test_words:
+    for word_type in unique_words:
         params['query'] = word_type
         url = service_url + '?' + urllib.urlencode(params)
         response = json.loads(urllib.urlopen(url).read())
